7b43ad01423f83cecf28e959a611e83c93eb1593
zplussz35 <15.zoltan.16@gmail.com>
2021-10-18 23:40:54 +0200

    Add Piece.java Board.java Move.java Alliance.java

--- ChessJavaProject/src/main/java/com/chess/engine/board/Tile.java
+++ ChessJavaProject/src/main/java/com/chess/engine/board/Tile.java
@@ -1,15 +1,16 @@
-package board;
+package com.chess.engine.board;
 
-import pieces.Piece;
+import com.chess.engine.pieces.Piece;
 
 import com.google.common.collect.ImmutableMap;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public abstract class Tile {
     protected final int tileCoordinate;
 
-    private static final Map<Integer,EmptyTile> EMPTY_TILES=createAllPossibleEmptyTiles();
+    private static final Map<Integer,EmptyTile> EMPTY_TILES_CACHE =createAllPossibleEmptyTiles();
 
     private static Map<Integer,EmptyTile> createAllPossibleEmptyTiles(){
         final Map<Integer,EmptyTile> emptyTileMap = new HashMap<>();
@@ -17,10 +18,15 @@ public abstract class Tile {
         for (int i = 0; i < 64; i++) {
             emptyTileMap.put(i,new EmptyTile(i));
         }
+
         return ImmutableMap.copyOf(emptyTileMap);
     }
 
-    Tile(int tileCoordinate){
+    public static Tile createTile(final int tileCoordinate, final Piece piece){
+        return piece != null ? new OccupiedTile(tileCoordinate,piece) : EMPTY_TILES_CACHE.get(tileCoordinate);
+    }
+
+    private Tile(int tileCoordinate){
         this.tileCoordinate=tileCoordinate;
     }
 
@@ -28,7 +34,7 @@ public abstract class Tile {
     public abstract Piece getPiece();
 
     public static final class EmptyTile extends Tile{
-        EmptyTile(final int coordinate){
+        private EmptyTile(final int coordinate){
             super(coordinate);
         }
 
@@ -46,7 +52,7 @@ public abstract class Tile {
 
     public static final class OccupiedTile extends Tile{
         private final Piece pieceOnTile;
-        OccupiedTile(final int tileCoordinate,Piece pieceOnTile){
+        private OccupiedTile(final int tileCoordinate,Piece pieceOnTile){
             super(tileCoordinate);
             this.pieceOnTile=pieceOnTile;
 
